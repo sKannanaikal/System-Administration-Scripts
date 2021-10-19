e1c578975f70b3f59b4cc7d7338e1d2b3cf8ba57
Sean Kannanaikal <stk5106@g.rit.edu>
2021-10-18 22:20:58 -0400

    Add files via upload

--- shortcut.py
+++ shortcut.py
@@ -26,6 +26,11 @@ def main():
 	destination = os.environ['HOME']
 	global LINKS
 	while True:
+		LINKS.clear()
+		fileListings = os.listdir()
+		for file in fileListings:
+			if(os.path.islink(file)):
+				LINKS.add(file)
 		choice = input("Enter an Option: ")
 		if(choice.lower() == 'quit'):
 			break
@@ -34,25 +39,22 @@ def main():
 			tokens = source.split('/')
 			fname = tokens[len(tokens) -1]
 			pipedResults = subprocess.run(["find", "/", "-type" ,"f", "-name", fname], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
-			if pipedResults == '':
+			if pipedResults.stdout.decode('utf-8') == '':
 				continue
+			print(pipedResults)
 			source = pipedResults.stdout.decode('utf-8').split('\n')[0]
+			print(source)
 			createLink(source, destination)
 		elif(choice == '2'):
 			source = input("Enter a source: ")
 			tokens = source.split('/')
 			fname = tokens[len(tokens) -1]
-			pipedResults = subprocess.run(["find", "/home/student", "-type" ,"f", "-name", fname], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
-			if pipedResults == '':
+			pipedResults = subprocess.run(["find", "/home/student", "-type" ,"l","-maxdepth", "1", "-name", fname], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
+			if pipedResults.stdout.decode('utf-8') == '':
 				continue
 			source = pipedResults.stdout.decode('utf-8').split('\n')[0]
 			deleteLink(source)
 		elif(choice == '3'):
-			file_listings = os.listdir()
-			for file in file_listings:
-				if(os.path.islink(file)):
-					LINKS.add(file)
-
 			print("Your current directory is {}".format(os.getcwd()))
 			print("The number of links is {}".format(str(len(LINKS))))
 			print("Symbolic Link\t\t\t\t\t\tTarget Path")
